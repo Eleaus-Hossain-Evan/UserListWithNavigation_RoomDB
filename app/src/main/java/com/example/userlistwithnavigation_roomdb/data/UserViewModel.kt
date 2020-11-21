5b25de99e35c7dc6bffb6c5c8231882ef28aab66
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2020-11-22 03:59:16 +0600

    Creat UserViewModel & UserRepositiory

---
+++ app/src/main/java/com/example/userlistwithnavigation_roomdb/data/UserViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.userlistwithnavigation_roomdb.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(application: Application):AndroidViewModel(application) {
+
+    private val readAllData: LiveData<List<User>>
+
+    private val repositiory: UserRepositiory
+
+    init {
+        val userDao = UserDatabase.getDatabase(application).userDao()
+        repositiory = UserRepositiory(userDao)
+
+        readAllData = repositiory.readAllData
+    }
+
+    fun addUser(user: User)= viewModelScope.launch(Dispatchers.IO) {
+        repositiory.addUser(user)
+    }
+
+
+}
